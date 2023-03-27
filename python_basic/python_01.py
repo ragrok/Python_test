d14be91208e417aadd76b68e5328304350fee156
李红健 <hongjian.li@patpat.com>
2023-03-27 20:08:25 +0800

    添加代码

---
+++ python_basic/python_01.py
@@ -0,0 +1,17 @@
+def my_abs(x) : 
+    if x >= 0:
+         return x
+    else:
+        return -x
+    
+def my_arry():
+    classmates = ['Michael', 'Bob', 'Tracy']
+    print(classmates[0])
+    print(classmates[1])
+    print(classmates[2])
+
+if __name__ == "__main__":
+    # my_abs(1)
+    # print(111)
+    my_arry()
+
